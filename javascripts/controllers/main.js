19bf97becbb4c82fbc8c4b93f18f280158d10fda
Jason Stiltner <jasonstiltner@gmail.com>
2015-12-14 16:04:46 -0600

    mvp achieved

--- javascripts/controllers/main.js
+++ javascripts/controllers/main.js
@@ -3,20 +3,61 @@ app.controller("MainCtrl", ["$scope", "$firebaseArray", "$firebaseObject", "auth
   var mealRef = new Firebase("https://bigfridge.firebaseio.com/meals");
   $scope.meals = $firebaseArray(mealRef);
 
+ var myNotificationRef = new Firebase("https://bigfridge.firebaseio.com/users/" + authData.getUid() + "/notifications/unconfirmed/");
+  $scope.notifications = $firebaseArray(myNotificationRef);
+
 
 
 
   $scope.reserveMeal = function (meal){
-//The below code marks switches the value at the key "reserved" 
+    // switches the value at the key "reserved" 
     meal.reserved = true
-//The below code updates the database with the reserved status.    
+    // updates the database with the reserved status.    
     $scope.meals.$save(meal);
 
     donorUser = meal.createdby;
-    var notificationRef = new Firebase("https://bigfridge.firebaseio.com/users/" + authData.getUid() + "/notifications/unconfirmed/");
+    var notificationRef = new Firebase("https://bigfridge.firebaseio.com/users/" + donorUser + "/notifications/unconfirmed/");
     
     $scope.notifications = $firebaseArray(notificationRef);
     $scope.notifications.$add(meal)
-  };
+    // console.log("scope.notifications", $scope.notifications );
+  };  // reserve meal function closes
+
+
+    //sandbox
+    var myNotificationRef = new Firebase("https://bigfridge.firebaseio.com/users/" + authData.getUid() + "/notifications/unconfirmed/");
+
+    var alertsRef = $firebaseArray(myNotificationRef);
+
+    
+     
+      
+     
+      console.log("$scope.alert", $scope.notifications );
+    
+
+
+    // list.$add({ hello: "world" });
+
+
+
 }]);
+    
+
+      // window.alert("your donation of", $scope.alert.name )
+      
+
+
+     
+
+
+
+
+
+
+
+    // logs { event: "child_added", key: "<new _id>", prevId: "<prev_id>" }
+
+
+
 
